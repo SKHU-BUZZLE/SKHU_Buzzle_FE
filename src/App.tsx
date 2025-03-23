7832c3975f0d02a999b7e728f877f243959c89f2
Kim, DongChan <kdc9050@naver.com>
2025-03-23 16:31:02 +0900

    Feat: 라우터 및 토큰 관리

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import LoginPage from "./pages/login/Login";
 import OpenApp from "./pages/loading/OpenApp";
 import PrivateRoute from "./routes/PrivateRoute";
 import OAuthCallback from "./pages/OAuthCallback";
+import { SinglePlay } from "./pages/SinglePlay";
 import Home from "./pages/Home";
+import PublicRoute from "./routes/PublicRoute";
 
 export default function App() {
   return (
-    <>
-      <Routes location={location}>
-        <Route path="/" element={<Layout />}>
-          <Route path="/loading" element={<OpenApp />} />
-          <Route path="home" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route element={<PublicRoute />}>
+          <Route path="loading" element={<OpenApp />} />
           <Route path="login" element={<LoginPage />} />
-          <Route
-            path="/api/oauth2/callback/kakao"
-            element={<OAuthCallback />}
-          />
+          <Route path="api/oauth2/callback/kakao" element={<OAuthCallback />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="home" element={<Home />} />
+          <Route path="singlePlay" element={<SinglePlay />} />
           <Route path="game" element={<div />} />
         </Route>
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 }
