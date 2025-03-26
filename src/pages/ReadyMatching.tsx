54ba453209a1640488004a96eedf6426278d3731
Kim, DongChan <kdc9050@naver.com>
2025-03-26 15:45:36 +0900

    Refactor: 멀티 플레이 준비 화면 리펙토링

--- src/pages/ReadyMatching.tsx
+++ src/pages/ReadyMatching.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { useAuthStore } from "../stores/authStore";
-import heart from "../assets/icons/heart.svg";
 
-import Pawn from "../assets/images/icons8-pawn-64.png";
 import { Navigate, useNavigate } from "react-router-dom";
 import { inGameState, useMultiMatchStore } from "../stores/multiStore";
+import { useUserStore } from "../stores/userStore";
+import TopStatusBar from "../components/TopStatusBar";
 
 const HomeImagesUrl = [
   "https://lottie.host/d245b7df-8f3e-4ddf-a40c-47115d14bd64/SFz8jCXuNp.lottie",
@@ -20,29 +19,25 @@ const MultiMent = [
 ];
 
 export default function ReadyMatching() {
-  const userName = "남규";
   const navigate = useNavigate();
-  const heartScore = 50;
   const setMultiState = useMultiMatchStore((state) => state.setState);
+  const { user, life, fetchUser, fetchLife } = useUserStore();
 
   const [randomLottie, setRandomLottie] = useState(HomeImagesUrl[0]);
   const [randomMent, setRandomMent] = useState(MultiMent[0]);
 
   useEffect(() => {
+    fetchUser();
+    fetchLife();
+
     const randomIndex = Math.floor(Math.random() * HomeImagesUrl.length);
     setRandomLottie(HomeImagesUrl[randomIndex]);
     setRandomMent(MultiMent[randomIndex]);
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      <header className="flex items-center justify-between px-4 py-3 border-b border-gray-200">
-        <img src={Pawn} alt="Chess" className="w-8 h-8" />
-        <div className="flex items-center gap-1">
-          <img className="w-7 h-7" src={heart} />
-          <span className="font-bold text-lg">{heartScore}</span>
-        </div>
-      </header>
+    <div className="flex flex-col min-h-screen w-full bg-white">
+      <TopStatusBar life={life} />
 
       <div className="flex-1 flex flex-col items-center justify-center gap-5 px-4 py-6">
         <h1 className="text-4xl font-bold text-center">다른 유저와 대전하기</h1>
