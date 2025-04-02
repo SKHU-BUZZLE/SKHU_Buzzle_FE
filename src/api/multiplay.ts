a4b885d7e5a26df842ce72d6bf2b1b525d883d33
lzvckaya <lzvckaya@gmail.com>
2025-04-02 14:42:23 +0900

    랭킹 적용 및 대기열 취소

--- src/api/multiplay.ts
+++ src/api/multiplay.ts
@@ -4,3 +4,8 @@ export const startMatching = () => {
   console.log("매칭시작");
   return axiosInstance.post("/match/v2");
 };
+
+export const cancelMatching = () => {
+  console.log("매칭 이탈");
+  return axiosInstance.post("/match/cancel/v2");
+};
